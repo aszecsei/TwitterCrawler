b3e31f76c14f0c289e67083efdabf84b272510eb
Alic Szecsei <Alic Szecsei>
2016-11-28 15:57:58 -0600

    Classification work

--- TwitterCrawl.py
+++ TwitterCrawl.py
@@ -33,7 +33,6 @@ def unix_time_millis(dt):
 bots = {}
 
 def getData(screenName):
-    print "Pulling data for " + screenName + "..."
     retweets = 0
     original = 0
     time.sleep(0.25)
@@ -44,7 +43,7 @@ def getData(screenName):
         else:
             original += 1
     usr = api.get_user(screenName)
-    return [retweets, original, usr.verified, unix_time_millis(usr.created_at), usr.default_profile, usr.default_profile_image, usr.favourites_count, usr.followers_count, usr.friends_count, usr.listed_count]
+    return [retweets, original, usr.verified, unix_time_millis(usr.created_at), usr.default_profile, usr.default_profile_image, usr.favourites_count, float(usr.followers_count) / float(usr.friends_count), usr.listed_count]
 
 def Similarity(msg1, msg2):
     return difflib.SequenceMatcher(a=seq1.lower(), b=seq2.lower()).ratio()
@@ -56,12 +55,11 @@ def TestSimilarity(msg1, msg2):
 def GenerateLearnSet():
     print "Generating learning set..."
     data = []
-    target = []
     with open("bots.txt", 'r') as botFile:
         for line in botFile:
             try:
                 mData = getData(line)
-                target.append(True)
+                mData.append(True)
                 data.append(mData)
             except:
                 pass
@@ -69,21 +67,19 @@ def GenerateLearnSet():
         for line in pplFile:
             try:
                 mData = getData(line)
-                target.append(False)
+                mData.append(False)
                 data.append(mData)
             except:
                 pass
     with open("learn.csv", 'w') as learnFile:
         writer = csv.writer(learnFile)
-        combinedData = list(data)
-        map(lambda (x,y): x.append(y), zip(combinedData, target))
-        for row in combinedData:
+        for row in data:
             writer.writerow(row)
-    return (data, target)
+    return data
 
 # uncomment if generating a learnset
-# (d, t) = GenerateLearnSet()
-
+d = GenerateLearnSet()
+'''
 d = []
 t = []
 with open("learn.csv", 'r') as learnFile:
@@ -103,20 +99,20 @@ with open("learn.csv", 'r') as learnFile:
         md.append(int(row[9]))
         d.append(numpy.array(md))
         t.append({'True':1.}.get(row[10], 0.))
+'''
+dd = numpy.array(d)[:,:-1]
+dt = numpy.array(d)[:,-1:]
 
-d = numpy.array(d)
-t = numpy.array(t)
-
-numRows = len(d)
-numCols = len(d[0])
-for r in d:
+numRows = len(dd)
+numCols = len(dd[0])
+for r in dd:
     if not len(r) == numCols:
         raise ValueError("Error! Array is jagged.")
-if not numRows == len(t):
+if not numRows == len(dt):
     raise ValueError("Error! Dataset doesn't have the same size as target set.")
 
 clf = svm.SVC()
-clf.fit(numpy.array(d), numpy.array(t))
+clf.fit(numpy.array(dd), numpy.array(dt))
 
 def Classify(screenName):
     dd = getData(screenName)
@@ -134,6 +130,19 @@ def AnalyzeFollowers(screenName):
                 print(friend.screen_name + " | " + str(isBot))
                 cfwriter.writerow([friend.screen_name, isBot])
 
+def AnalyzeMentions(screenName, forceLinks=False):
+    tList = tweepy.Cursor(api.search, q=("@" + screenName + (" filter:links" if forceLinks else ""))).items(200)
+    numBots = 0
+    numPpl = 0
+    for tweet in tList:
+        isBot = Classify(tweet.user.screen_name)
+        print tweet.user.screen_name + (": BOT" if int(round(isBot)) == 1 else ": PERSON")
+        if int(round(isBot)) == 1:
+            numBots += 1
+        else:
+            numPpl += 1
+    print "BOTS: " + str(numBots) + " | PPL: " + str(numPpl)
+
 def AnalyzeSQLTable(sqlfile):
     print "Opening SQL database..."
     conn=sqlite3.connect(sqlfile)
@@ -145,8 +154,22 @@ def AnalyzeSQLTable(sqlfile):
     except:
         pass
     curse.execute("select * from udata")
+    numBot = 0
+    numPpl = 0
     for row in curse:
         print "Analyzing @" + row[0] + "..."
+        try:
+            isBot = Classify(row[0])
+            if int(round(isBot)) == 0:
+                numPpl += 1
+            else:
+                numBot += 1
+            print row[0] + ": " + ("Bot" if int(round(isBot)) == 1 else "Person")
+        except Exception as err:
+            print "Error retrieving data for " + row[0] + " | " + str(err)
+    print "BOTS: " + str(numBot) + " | PPL: " + str(numPpl)
+
+    '''
         curse2 = conn.cursor()
         # This isn't working for some reason - using Classify instead
         # data = [row[1], row[2], (row[3] == 1), unix_time_millis(datetime.datetime.strptime(row[4], '%Y-%m-%d %H:%M:%S')), (row[5] == 1), (row[6] == 1), row[7], row[8], row[9], row[10]]
@@ -158,9 +181,11 @@ def AnalyzeSQLTable(sqlfile):
             conn.commit()
         except Exception as err:
             print "Error inserting data for " + row[0] + " | " + str(err)
+        '''
     conn.close()
 
 # AnalyzeFollowers("uflaz11")
 # AnalyzeFollowers("Djawadi_Ramin")
-#AnalyzeFollowers("NancyBadillo13")
-AnalyzeSQLTable("udataf3.sqlite")
+# AnalyzeFollowers("NancyBadillo13")
+# AnalyzeSQLTable("udataf.sqlite")
+AnalyzeMentions("NathanFillion")
